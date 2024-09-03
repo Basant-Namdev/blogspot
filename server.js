48fa1d6c080418621b5bad843565e6851e3a8125
Basant Namdev <princenamdev3134@gmail.com>
2024-09-03 21:15:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  try {
+    exports.dbConnection = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("database connected");
+  } catch (err) {
+    console.error("Error connecting to database:", err);
+  }
+}
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static("public"));
+server.use('/public',express.static("public"));
+
+
+
+server.listen(process.env.PORT, () => { console.log("Server is running ") });
